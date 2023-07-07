3edc46070e6b7833bfe8727952a02a9950bb747a
tonilocust <44360593+tonilocust@users.noreply.github.com>
2023-07-07 20:39:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/git_update', methods=['POST'])
+
+def git_update():
+    repo = git.Repo('./pythonanywhere')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 def home_page():
     message="Welcome to the Home Page"
