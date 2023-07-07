e4045477b807c13c3949c5317c8a98d05d8999d2
tonilocust <44360593+tonilocust@users.noreply.github.com>
2023-07-07 19:49:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,12 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Cargamos el modelo
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
+@app.route('/', methods=['POST'])
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.json
-    prediction = model.predict(np.array(data['example']).reshape(1, -1))
-    return jsonify({'prediction': int(prediction[0])})
+def home_page():
+    message="Welcome to the Home Page"
+    response={"message":message}
+    return jsonify({"response":response}), 200
 
 if __name__ == '__main__':
     app.run()
