38809daff69af64bf5bfe120c2c675dccf956672
tonilocust <44360593+tonilocust@users.noreply.github.com>
2023-07-07 20:40:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/git_update', methods=['POST'])
 
 def git_update():
-    repo = git.Repo('./pythonanywhere')
+    repo = git.Repo('./flask_pythonanywhere')
     origin = repo.remotes.origin
     repo.create_head('main',
                      origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
